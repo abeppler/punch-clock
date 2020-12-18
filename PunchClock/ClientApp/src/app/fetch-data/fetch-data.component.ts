e2722390c0a22fb87f16fb417e4a283d59baf095
abeppler <abeppler2000@gmail.com>
2020-12-18 10:08:47 -0300

    binding punches list

--- PunchClock/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ PunchClock/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -8,8 +8,15 @@ import { HttpClient } from '@angular/common/http';
 export class FetchDataComponent {
   public forecasts: WeatherForecast[];
   public employees: Employee[];
+  public punches: Punch[];
+  
+  private _baseUrl: string;
+  private _httpClient : HttpClient;
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    this._baseUrl = baseUrl;
+    this._httpClient = http;
+
     http.get<WeatherForecast[]>(baseUrl + 'weatherforecast')
       .subscribe(result => {
         this.forecasts = result;
@@ -18,8 +25,21 @@ export class FetchDataComponent {
     http.get<Employee[]>(baseUrl+'employee')
         .subscribe(result => {
           this.employees = result;
+
+          if (this.employees.length > 0) {
+            this.fetchPunches(this.employees[0].id);
+          }
+
         }, error => console.error(error));
-  }    
+  }
+
+  fetchPunches(employeeId: string){
+    this._httpClient.get<Punch[]>(this._baseUrl+`punch/${employeeId}`)
+      .subscribe(result => {
+        this.punches = result;
+        console.table(this.punches);
+      }, error => console.error(error));    
+  }
 }
 
 interface WeatherForecast {
@@ -32,4 +52,11 @@ interface WeatherForecast {
 interface Employee {
   id: string;
   name: string;
+}
+
+interface Punch {
+  employeeId: string;
+  employeeName: string;
+  datetime: Date;
+  punchType: string;
 }
